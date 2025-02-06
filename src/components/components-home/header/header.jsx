7a360f29e0491a7369608483dec4cbdc2923d9d4
dantauu <azazzzz455u@gmail.com>
2025-02-06 12:52:45 +0300

    more fixing

--- src/components/components-home/header/header.jsx
+++ src/components/components-home/header/header.jsx
@@ -12,7 +12,7 @@ import './header.css'
 
 
 const Header = () => {
-	const isMobile = useMediaQuery('(max-width: 890px)')
+	const isMobile = useMediaQuery('(max-width: 1100px)')
 	const [aciveMenu, setActiveMenu] = useState(false)
 	const { setDropHeader } = useContext(DropHeaderContext)
 	const { aciveInputFunc } = useContext(InputContext)
